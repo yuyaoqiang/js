5d641a783b5fd2e543182ef61c552bdd958c7984
Tony Yu (SRD) <tony.y@tc-gaming.co>
2018-11-26 19:15:50 +0800

    头部

--- src/containers/header/index.jsx
+++ src/containers/header/index.jsx
@@ -6,9 +6,19 @@ class Header extends React.PureComponent {
       <React.Fragment>
         <header className="header-wrap">
           <div className="header-content">
-            <div className="header-left-title">简数</div>
-            <div className="header-input">首页</div>
-            <div className="header-rigth-logoin">写文章</div>
+            <div className="header-left-title">
+              <img src="http://cdn2.jianshu.io/assets/web/nav-logo-4c7bbafe27adc892f3046e6978459bac.png" alt="简书"/>
+            </div>
+            <div className="header-container">
+              123
+              <a href=""></a>
+              <a href=""></a>
+
+            </div>
+            <div className="header-rigth-logoin">
+                <span className="login">注册</span>
+                <span className="write">写文章</span>
+            </div>
           </div>
         </header>
       </React.Fragment>
